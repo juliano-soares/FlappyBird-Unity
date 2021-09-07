9fbec0510d1b70a537b361ab7db2739212f1d365
juliano-soares <37117169+juliano-soares@users.noreply.github.com>
2021-09-07 14:10:21 -0300

    Finalizado o game

---
+++ Assets/GameFolder/Scripts/PipeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * -2.5f * Time.deltaTime;
+    }
+}
---
+++ Assets/GameFolder/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Text scoreView;
+    public Transform gameOver;
+    public float jump;
+
+    [SerializeField]
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jump;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("AddScore"))
+        {
+            score++;
+            scoreView.text = score.ToString();
+        }
+
+        if(collision.CompareTag("Pipe"))
+        {
+            enabled = false;
+            gameOver.gameObject.SetActive(true);
+            Invoke("Pause", 2);
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+    }
+}
